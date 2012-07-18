4a13e1ecb7e525e17db2a8347e2179dd30c00b1b
Dan Matthews <dan@hydrant.co.uk>
2012-07-18 18:16:11 +0100

    base classes

---
+++ lib/Sirportly.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Imports all files required for full Sirportly library access.
+ */
+ include_once(__DIR__.'/SirportlyConfiguration.php');
+ include_once(__DIR__.'/SirportlyTickets.php');
---
+++ lib/SirportlyConfiguration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provides Sirportly API constants in an OOP way.
+ */
+class SirportlyConfiguration {
+
+	const SIRPORTLY_API_KEY = '';
+	const SIRPORTLY_API_URL = 'https://api.sirportly.com/';
+
+}
