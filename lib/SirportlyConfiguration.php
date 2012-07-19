ad24e886f7b637cd2a1d90ce2d838a0a43d1ced5
Dan Matthews <dan@danmatthews.me>
2012-07-19 10:37:12 +0200

    Removed STUPID API KEYS

--- lib/SirportlyConfiguration.php
+++ lib/SirportlyConfiguration.php
@@ -4,8 +4,8 @@
  */
 class SirportlyConfiguration {
 
-	const SIRPORTLY_API_TOKEN = 'bed82429-66a4-827f-e95d-931e087b7d9f';
-	const SIRPORTLY_API_SECRET = 'oyhuu34exesb7ne8lu4j436bqy8o0mcfzygvu4vf6bn1lxfw5n';
+	const SIRPORTLY_API_TOKEN = '';
+	const SIRPORTLY_API_SECRET = '';
 	const SIRPORTLY_API_URL = 'https://api.sirportly.com/';
 
 }
